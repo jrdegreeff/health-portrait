355b57a7a0f1ee48b600171f749254e5f84f27be
Olivia Giandrea <og102@mit.edu>
2022-11-21 23:54:11 -0500

    implement boilerplate frontend

--- client/components/HomePage.vue
+++ client/components/HomePage.vue
@@ -1,5 +1,18 @@
 <template>
     <main>
         <h1>Hello World</h1>
+        <p>Some example paragraph text</p>
+
+        <button class="btn btnPrimary">Primary Button</button>
+        <button class="btn btnSecondary">Secondary Button</button>
+        <button class="btn btnTertiary">Tertiary Button</button><br /><br />
+
+        <fieldset>
+            <legend>An Example Form</legend>
+            <label for="fName">First Name:</label>
+            <input type="text" name="fName" placeholder="Daniel"><br />
+            <label for="fName">Last Name:</label>
+            <input type="text" name="fName" placeholder="Jackson">
+        </fieldset>
     </main>
 </template>
