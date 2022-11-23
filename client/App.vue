4ce590d17dc1e7441671e256a553e3aed25142ba
Olivia Giandrea <og102@mit.edu>
2022-11-22 22:54:24 -0500

    implement boilerplate frontend (#1)
    * implement boilerplate frontend
    
    * move css to app.vue, add sidenav

--- client/App.vue
+++ client/App.vue
@@ -19,8 +19,158 @@ export default {
   box-sizing: border-box;
 }
 
+:root {
+  --primaryGray: #d9d9d9;
+  --lightPrimaryGray: #e3e3e3;
+  --secondaryGray: #d9d9d9aa;
+  --lightSecondaryGray: #e3e3e3aa;
+  --darkGray: #4f4f4f;
+}
+
 body {
   height: 100vh;
+  font-family: 'Inter', sans-serif;
   margin: 0;
+  font-size: 1.1rem;
+  padding: 2rem;
 }
+
+h1 {
+  margin-top: 0rem;
+}
+
+@media (max-width: 30rem) {
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+}
+
+.sidenav {
+  height: 100%; 
+  width: 30%; 
+  position: fixed; 
+  z-index: 1; 
+  top: 0; 
+  left: 0;
+  background-color: var(--primaryGray); 
+  overflow-x: hidden; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+  padding: 1rem 1rem 1rem 3rem;
+}
+
+.sidenav h1 {
+  font-weight: 600;
+}
+
+.sidenav a {
+  text-decoration: none;
+  color: black;
+}
+
+.navGroup ul {
+  margin: 0rem !important;
+}
+
+.navGroup li {
+  padding-bottom: 1rem;
+  text-align: left;
+  font-size: 1.4rem;
+}
+
+.navItem {
+  color: black;
+}
+
+.navItem:hover {
+  color: var(--darkGray);
+  cursor: pointer;
+}
+
+.content {
+  margin-left: 32% !important;
+  width: 66%;
+}
+
+@media (max-width: 60rem) {
+  .main, #app {
+    display: flex;
+    justify-content: center;
+    margin: 1rem;
+  }
+
+  .content {
+    width: 90%;
+    text-align: center;
+    margin: auto !important;
+  }
+}
+
+@media (min-width: 60rem) {
+
+}
+
+.btn {
+  border: 1px solid black;
+  border-radius: 1rem;
+  padding: 0.5rem 2rem;
+  margin: 1rem;
+}
+
+.btn:hover {
+  cursor: pointer;
+}
+
+.btnPrimary {
+  background-color: var(--primaryGray);
+}
+
+.btnPrimary:hover {
+  background-color: var(--lightPrimaryGray);
+}
+
+.btnSecondary {
+  background-color: var(--secondaryGray);
+  border: none;
+  padding: 1rem 2rem;
+}
+
+.btnSecondary:hover {
+  background-color: var(--lightSecondaryGray);
+}
+
+.btnTertiary {
+  background-color: white;
+  border-radius: 15px;
+  padding: 0.5rem 0.7rem;
+}
+
+.btnTertiary:hover {
+  background-color: var(--lightSecondaryGray);
+}
+
+fieldset {
+  border: 1px solid black;
+}
+
+legend {
+  font-weight: bold;
+}
+
+label {
+  margin-right: 1rem;
+}
+
+input {
+  border-radius: 0.5rem;
+  border: 1px solid black;
+  padding: 0.5rem;
+  margin: 0.5rem;
+}
+
 </style>
