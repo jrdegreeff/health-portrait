6ec4e83221b14c57451918b62e482b620fd07d6f
isabellapedraza <79341675+isabellapedraza@users.noreply.github.com>
2022-12-01 17:29:39 -0500

    medical contact backend (#11)
    * medical contact model, collection, and middleware complete
    
    * medical contact util complete
    
    * medical contact backend complete
    
    * pr changes addressed
    
    * medical contact backend complete, all pr changes addressed

--- api/index.ts
+++ api/index.ts
@@ -9,6 +9,7 @@ import dotenv from 'dotenv';
 import MongoStore from 'connect-mongo';
 import * as accountValidator from '../server/account/middleware';
 import {accountRouter} from '../server/account/router';
+import { medicalContactRouter } from "../server/medical-contact/router";
 
 // Load environmental variables
 dotenv.config({});
@@ -71,6 +72,7 @@ app.use(accountValidator.isCurrentSessionAccountExists);
 
 // Add routers from routes folder
 app.use('/api/accounts', accountRouter);
+app.use('/api/medical-contacts', medicalContactRouter);
 
 const isProduction = process.env.NODE_ENV === 'production';
 const vuePath = path.resolve(__dirname, "..", "client", isProduction ? "dist" : "public");
