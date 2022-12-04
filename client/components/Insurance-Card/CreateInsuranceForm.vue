d81a11e2c24ad6103847a3ab78d0f3e5e9f84c2e
isabellapedraza <79341675+isabellapedraza@users.noreply.github.com>
2022-12-04 14:08:43 -0500

    one small fix

--- client/components/Insurance-Card/CreateInsuranceForm.vue
+++ client/components/Insurance-Card/CreateInsuranceForm.vue
@@ -10,12 +10,12 @@ export default {
       method: 'POST',
       hasBody: true,
       fields: [
+        {id: 'purpose', label: 'Purpose', value: ''},
         {id: 'subscriber_name', label: 'Subscriber Name', value: ''},
         {id: 'member_id', label: 'Member ID', value: ''},
         {id: 'group_number', label: 'Group Number', value: ''},
         {id: 'plan_number', label: 'Plan Number', value: ''},
         {id: 'plan_type', label: 'Plan Type', value: ''},
-        {id: 'purpose', label: 'Purpose', value: ''},
         {id: 'notes', label: 'Notes', value: ''},
       ],
       title: 'Add new insurance card',
