4304be8188725bf0677f6c0c4caeb3086bdcf37b
isabellapedraza <79341675+isabellapedraza@users.noreply.github.com>
2022-12-10 21:24:51 -0500

    complete

--- client/components/Insurance-Card/InsurancePage.vue
+++ client/components/Insurance-Card/InsurancePage.vue
@@ -13,9 +13,17 @@
     <section>
       <header>
         <h2>Current Insurance Cards</h2>
+        <div class="search-wrapper">
+          <label>Search by purpose:</label>
+          <input
+            v-model="search"
+            type="text"
+            placeholder="Search insurances..."
+          >
+        </div>
       </header>
       <InsuranceCard
-        v-for="insurance in $store.state.insurances"
+        v-for="insurance in filteredInsurances"
         :key="insurance._id"
         :document="insurance"
       />
@@ -32,6 +40,19 @@
     components: {
       CreateInsuranceForm, 
       InsuranceCard
+    },
+    data() {
+      return {
+        search: '',
+      };
+    },
+    computed: {
+      // Inspired by https://codepen.io/AndrewThian/pen/QdeOVa
+      filteredInsurances() {
+        return this.$store.state.insurances.filter(insurance => {
+          return insurance.purpose.toLowerCase().includes(this.search.toLowerCase())
+        })
+      }
     },
     mounted() {
       this.$store.commit("setHeader", {
